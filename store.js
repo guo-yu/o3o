2686557dd3721d8693ec7b67ed1215f37e1058a4
guo-yu <o.u.turing@gmail.com>
2013-07-31 01:15:32 +0800

    fully tested

---
+++ store.js
@@ -0,0 +1,21 @@
+// store
+var fs = require('fs'),
+    Store = JSON.parse(fs.readFileSync('./store.json'));
+    
+exports.check = function(type) {
+    if (type && typeof(type) == 'string') {
+        var list = [];
+        for (var key in Store) {
+            list.push(key);
+        };
+        if (list.indexOf(type) > -1) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        return false;
+    }
+}
+
+exports.list = Store;
