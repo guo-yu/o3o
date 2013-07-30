1953c9231df2e2946f5446b0c183185ddd49963a
Guo Yu <i@guoyu.me>
2013-07-31 01:50:09 +0800

    CLI supported, ship to 0.0.2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 // ascii emoticon store based on Node.js
 
 var store = require('./store'),
+    optimist = require('optimist'),
+    argv = optimist.argv,
     Chance = require('chance');
 
 exports.fetch = function(type) {
@@ -15,5 +17,20 @@ exports.fetch = function(type) {
             chance = new Chance(),
             one = chance.integer({min: 0, max: list.length - 1});
         return list[one];
-    };
+    } else {
+        return false;
+    }
+}
+
+exports.cli = function() {
+    var type = argv._;
+    if (type.length == 1) {
+        var s = exports.fetch(type[0]);
+        if (s) {
+            console.log(s);
+            return false;
+        } else {
+            console.log('╮(╯_╰)╭ 没有这个情绪类别哦。。要不你帮我加上？欢迎fork && PR：https://github.com/turingou/o3o')
+        }
+    }
 }
