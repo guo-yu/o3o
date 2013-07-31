aa08f970609d20a56baece2f79f761754ec87344
flynngao <flynn.gao.y@gmail.com>
2013-07-31 17:08:12 +0800

    multi

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var store = require('./store'),
     optimist = require('optimist'),
     argv = optimist.argv,
     Chance = require('chance');
+    color = require('cli-color');
 
 exports.fetch = function(type) {
     if (store.check(type)) {
@@ -31,20 +32,27 @@ exports.available = function() {
 
 exports.cli = function() {
     var type = argv._;
-    var fail = exports.fetch('摊手') + '没有这个情绪类别哦。。要不你帮我加上？欢迎fork && PR：https://github.com/turingou/o3o';
-    if (type.length == 1) {
-        if (type[0] == 'ls') {
+    var fail = exports.fetch('摊手') + '没有这个情绪类别哦。。要不你帮我加上？欢迎fork && PR：https://github.com/turingou/o3o' + "\n 用ls或list看看现在有什么哦";
+    if (type.length >= 1) {
+        if (type[0] == 'ls' || type[0] == 'list') {
             console.log(exports.available())
         } else {
-            var s = exports.fetch(type[0]);
-            if (s) {
-                console.log(s);
-                return false;
-            } else {
-                console.log(fail)
-            }
+            var print = "";
+            for(var i in type){
+                
+                var s = exports.fetch(type[i]);
+                if (s) {
+                    print += (s + "\n");
+                } else {
+                    console.log(fail)
+                    return false;
+                }
+                
+            }    
+            console.log(print);
         }
     } else {
         console.log(fail)
+
     }
 }
