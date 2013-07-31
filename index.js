c816944e7177315a50e8c72690b7a23f08b49776
flynngao <flynn.gao.y@gmail.com>
2013-07-31 17:18:40 +0800

    multi

--- index.js
+++ index.js
@@ -32,24 +32,33 @@ exports.available = function() {
 
 exports.cli = function() {
     var type = argv._;
+
     var fail = exports.fetch('摊手') + '没有这个情绪类别哦。。要不你帮我加上？欢迎fork && PR：https://github.com/turingou/o3o' + "\n 用ls或list看看现在有什么哦";
     if (type.length >= 1) {
         if (type[0] == 'ls' || type[0] == 'list') {
             console.log(exports.available())
         } else {
-            var print = "";
+            
             for(var i in type){
-                
+                var c = 15;
+                if(argv.c == 'r'){
+                    chance = new Chance(),
+                    c = chance.integer({
+                        min: 0,
+                        max: 231
+                    });
+                }
+
                 var s = exports.fetch(type[i]);
                 if (s) {
-                    print += (s + "\n");
+                    console.log(color.xterm(c)(s));
                 } else {
                     console.log(fail)
                     return false;
                 }
                 
             }    
-            console.log(print);
+            
         }
     } else {
         console.log(fail)
