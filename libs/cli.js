741daa539faf317872c5a3cfbd52e9286a26671b
guo-yu <o.u.turing@gmail.com>
2013-12-13 23:45:48 +0800

    0.0.6: format code

--- libs/cli.js
+++ libs/cli.js
@@ -1,27 +1,23 @@
-var optimist = require('optimist'),
+var readline = require('readline'),
+    optimist = require('optimist'),
+    iconv = require('iconv').Iconv;
     argv = optimist.argv,
     o3o = require('../index');
 
 var convertToGBK = function(s) {
-    var Iconv = require('iconv').Iconv;
-    var iconv = new Iconv('UTF-8', 'GBK//IGNORE');
-    return iconv.convert(s);
+    return new iconv('UTF-8', 'GBK//IGNORE').convert(s);
 };
 
 var cliResponse = function(type) {
     var fail = o3o.fetch('摊手') + '没有这个情绪类别哦。。要不你帮我加上？欢迎fork && PR：https://github.com/turingou/o3o';
     if (type == 'ls') {
         var s = JSON.stringify(o3o.available(), null, 4);
-        if (argv.gbk) {
-            s = convertToGBK(s);
-        }
+        if (argv.gbk) s = convertToGBK(s);
         process.stdout.write(s);
     } else {
         var s = o3o.fetch(type);
         if (s) {
-            if (argv.gbk) {
-                s = convertToGBK(s)
-            }
+            if (argv.gbk) s = convertToGBK(s);
             process.stdout.write(s);
         } else {
             console.log(fail);
@@ -31,7 +27,6 @@ var cliResponse = function(type) {
 
 exports = module.exports = function() {
     if (argv.stdin) {
-        var readline = require('readline');
         var rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
--- libs/store.js
+++ libs/store.js
@@ -1,16 +1,16 @@
 // store
 var yans = require('../yan.json').list;
 
-exports.createList = function() {
+exports.list = function() {
     var Store = {};
     Store['available'] = [];
-    yans.forEach(function(l) {
-        Store.available.push(l.tag);
-        l.tag.split(" ").forEach(function(ll) {
-            if (typeof Store[ll] == 'undefined') {
-                Store[ll] = l.yan;
+    yans.forEach(function(line) {
+        Store.available.push(line.tag);
+        line.tag.split(" ").forEach(function(tag) {
+            if (typeof Store[tag] == 'undefined') {
+                Store[tag] = line.yan;
             } else {
-                Store[ll] = Store[ll].concat(l.yan);
+                Store[tag] = Store[tag].concat(line.yan);
             }
         });
     });
@@ -18,8 +18,6 @@ exports.createList = function() {
 };
 
 exports.check = function(type) {
-    var Store = exports.createList();
+    var Store = exports.list();
 	return Object.prototype.hasOwnProperty.call(Store, type);
 };
-
-exports.list = exports.createList();
