70a78d90bdbea0e57e86ebf1d136a088f775665e
guo-yu <o.u.turing@gmail.com>
2015-07-26 14:52:16 +0800

    Bugfix, fix a typo in lib o3o

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 import should from 'should'
 import o3o from '../dist/o3o'
-import { list } from '../yan.json'
+import yans from '../yan.json'
 
 describe('Available', () => {
   it('should return a available list', done => {
     const list = o3o('tags')
     list.should.be.an.instanceof(Array)
-    list.length.should.be.equal(list.length)
+    list.length.should.be.equal(yans.list.length)
     done()
   })
 })
